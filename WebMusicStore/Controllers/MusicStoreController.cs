b11e4523a042839c0d6ef671f84cb6382fb72e81
BuildTools <unconfigured@null.spigotmc.org>
2023-10-26 10:18:02 -0500

    Store Music

---
+++ WebMusicStore/Controllers/MusicStoreController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebMusicStore.Controllers
+{
+    public class MusicStoreController : Controller
+    {
+        // GET: MusicStore
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Pop()
+        {
+            return View();
+        }
+
+        public ActionResult Urbano()
+        {
+            return View();
+        }
+
+        public ActionResult ClassicRap()
+        {
+            return View();
+        }
+    }
+}
